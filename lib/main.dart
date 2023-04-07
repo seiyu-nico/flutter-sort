dd1434c2c92fd4bf253e9746b2848dcda145dac9
seiyu-nico <yuseiishii141@gmail.com>
2023-04-07 22:22:42 +0900

    デバッグバー削除

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text('Selection Sort')),
         body: const Home(),
